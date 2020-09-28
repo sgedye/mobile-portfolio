139c27b8d4fac42cb35ddcccf9755b5ba6c5aac3
Shaun Gedye <sgedye88@yahoo.com.au>
2020-09-28 20:35:11 +0800

    Updated Header
    Removed the React-Bootstrap to get toggling working properly.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,75 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
-import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
+import { Link, useLocation } from "react-router-dom";
 
 export const Header = () => {
   const { pathname } = useLocation();
   const title = pathname.substring(1) || "Home";
+  const [isNavCollapsed, setIsNavCollapsed] = React.useState(true);
+
+  const toggleNav = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
-    <header className="bg-primary sticky-top header">
-      <Navbar bg="" expand="lg">
-        <Navbar.Brand href="/">
-          <img src="/content/gfx/sg-logo.png" alt="SG Logo" className="img-fluid w-20" />
-        </Navbar.Brand>
-        <Navbar.Text className="h3 justify-content-center text-uppercase m-0">
+    <header className="sticky-top header">
+      <nav className="navbar navbar-expand-lg navbar-light  bg-primary">
+        <Link className="navbar-brand text-info font-weight-bolder" to="/">
+          <img
+            src="/content/gfx/sg-logo.png"
+            alt="SG Logo"
+            className="img-fluid w-20"
+          />
+        </Link>
+        <h1 className="h3 justify-content-center text-uppercase m-0">
           {title}
-        </Navbar.Text>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link1</Nav.Link>
-            <Nav.Link href="#link">Link2</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+        </h1>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#topNavBar"
+          aria-controls="topNavBar"
+          aria-expanded={!isNavCollapsed}
+          aria-label="Toggle navigation"
+          onClick={toggleNav}
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className={`${
+            isNavCollapsed ? "collapse" : ""
+          } navbar-collapse justify-content-end`}
+          id="topNavBar"
+        >
+          <Link
+            className="nav-link text-center text-white"
+            onClick={toggleNav}
+            to="/"
+          >
+            Home
+          </Link>
+          <Link
+            className="nav-link text-center text-white"
+            onClick={toggleNav}
+            to="/about"
+          >
+            About
+          </Link>
+          <Link
+            className="nav-link text-center text-white"
+            onClick={toggleNav}
+            to="/projects"
+          >
+            Projects
+          </Link>
+          <Link
+            className="nav-link text-center text-white"
+            onClick={toggleNav}
+            to="/contact"
+          >
+            Contact
+          </Link>
+        </div>
+      </nav>
     </header>
   );
 };
