2c31d854bb250f7ed324f6104153e0ea0603d591
Shaun Gedye <sgedye88@yahoo.com.au>
2020-09-28 18:09:05 +0800

    create projects page

---
+++ src/assets/data/projectData.ts
@@ -0,0 +1,23 @@
+export const projectData = [
+  {
+    title: "Optica Medica",
+    url: "https://www.opticamedicabuga.com/",
+    img: "/content/gfx/optica.png",
+    blurb: "A complete revamp of an old website, which was unresponsive and unreadable on mobile devices.",
+    // languages: "Photoshop | JavaScript | HTML | CSS | SCSS"
+  },
+  {
+    title: "RTG Photography",
+    url: "https://www.rtgphotography.com.au/",
+    img: "/content/gfx/rtg-photography.png",
+    blurb: "A bespoke, responsive website designed and created for a local travel photographer",
+    // languages: "Photoshop | Carva | UI Kit | PHP | HTML | CSS | cPanel | Google Analytics"
+  },
+  {
+    title: "Mini Projects",
+    url: "https://sgedye.github.io/mini-projects/",
+    img: "/content/gfx/mini-projects.png",
+    blurb: "Various projects undertaken for personal interest, continuous learning and professional development",
+    // languages: "React, JavaScript, jQuery, Syled-Components, Bootstrap, HTML, CSS, SCSS, Photoshop"
+  }
+]
