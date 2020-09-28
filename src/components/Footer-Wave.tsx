05dd48d139d13246d6b8f018695dfccc40e28dd3
Shaun Gedye <sgedye88@yahoo.com.au>
2020-09-28 16:56:43 +0800

    Added Footer Wave :art:

---
+++ src/components/Footer-Wave.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const FooterWave = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    data-name="bottom-wave"
+    preserveAspectRatio="none"
+    viewBox="0 0 1200 120"
+    style={{ position: "relative", top: "1px" }}
+  >
+    <path
+      d="M600 112.77C268.63 112.77 0 65.52 0 7.23V120h1200V7.23c0 58.29-268.63 105.54-600 105.54z"
+      fill="#007bff"
+    ></path>
+  </svg>
+);
